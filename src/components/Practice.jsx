87ae508305665e3d85b2dada978d5612608d9204
ovara1mark <markovara1@gmail.com>
2022-08-09 23:28:43 +0100

    second update

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from "react";
 
 
-const Button = (props) =>{
-    const handleClick = ()=>props.num(props.incrementValue)
-    return(
+const Button = (props) => {
+    const handleClick = ()=> props.num(props.incrementValue)
+    return (
         <>
         <button
         onClick={handleClick}
@@ -14,25 +14,29 @@ const Button = (props) =>{
     )
 }
 
-const Display = (props) => <div>{props.message}</div>
+const Display = props => <div>{props.message}</div>
+
 
 export const Practice = () =>{
-    const [count, setCounter] = useState(0)
-    const incrementation = (increased)=>setCounter(count + increased)
-    const decrementation = (increased)=> setCounter(count + increased)
-
-    return(
+    const numb ={
+        vat:"this is an object"
+    }
+    const [count, setCounter] = useState(numb.vat)
+    const incrementation = (increased) => setCounter(count + increased)
+   
+    return (
         <>
         <Button num={incrementation} incrementValue={1}/>
         <Button num={incrementation} incrementValue={2}/>
         <Button num={incrementation} incrementValue={3}/>
         <Button num={incrementation} incrementValue={4}/>
 
-        <Button num={decrementation} incrementValue={-1}/>
-        <Button num={decrementation} incrementValue={-2}/>
-        <Button num={decrementation} incrementValue={-3}/>
-        <Button num={decrementation} incrementValue={-4}/>
+        <Button num={incrementation} incrementValue={-1}/>
+        <Button num={incrementation} incrementValue={-2}/>
+        <Button num={incrementation} incrementValue={-3}/>
+        <Button num={incrementation} incrementValue={-4}/>
         <Display message={count} />
         </>
     )
 }
+
